df9f1849597fd950ced23bdd9471db5a87973f65
felixrigau <felixrigau88@gmail.com>
2023-10-16 17:31:02 +0200

    refactor repository

--- src/infra/adapters/ItemInMemoryRepository.ts
+++ src/infra/adapters/ItemInMemoryRepository.ts
@@ -3,69 +3,57 @@ import { ItemRespository } from "../../model/IItemRepository";
 import { LOCAL_STORAGE_KEY } from "./constants";
 import { DataBaseSchema } from "./types";
 
+const getLocalData = (): DataBaseSchema => {
+  const db = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  if (db) {
+    return JSON.parse(db);
+  } else {
+    const initialDB: DataBaseSchema = { items: [] };
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(initialDB));
+
+    return initialDB;
+  }
+};
+
+const updateLocalData = (newData: Partial<DataBaseSchema>) => {
+  const db = getLocalData();
+
+  localStorage.setItem(
+    LOCAL_STORAGE_KEY,
+    JSON.stringify({ ...db, ...newData })
+  );
+};
+
 export class ItemInMemoryRepository implements ItemRespository {
   save = (item: Item) => {
-    const db = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (db) {
-      const randomlyDB: DataBaseSchema = JSON.parse(db);
+    const randomlyDB = getLocalData();
 
-      randomlyDB.items.push(item);
+    randomlyDB.items.push(item);
 
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(randomlyDB));
-    }
+    updateLocalData(randomlyDB);
   };
 
   remove = (id: string) => {
-    const db = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (db) {
-      const randomlyDB = JSON.parse(db);
-
-      localStorage.setItem(
-        LOCAL_STORAGE_KEY,
-        JSON.stringify({
-          ...randomlyDB,
-          items: randomlyDB.items.filter((item: Item) => item.id !== id),
-        })
-      );
-    }
+    updateLocalData({
+      items: getLocalData().items.filter((item: Item) => item.id !== id),
+    });
   };
 
-  getAll = (): Item[] => {
-    const db = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (db) {
-      const randomlyDB = JSON.parse(db);
+  getAll = (): Item[] => getLocalData().items;
 
-      return randomlyDB.items;
-    }
-  };
-
-  findBy = (id: string): Item => {
-    const db = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (db) {
-      const randomlyDB = JSON.parse(db);
-
-      return randomlyDB.items.find((item: Item) => item.id === id);
-    }
-  };
+  findBy = (id: string): Item =>
+    getLocalData().items.find((item: Item) => item.id === id);
 
   update = (id: string, updateItem: Partial<Item>): void => {
-    const db = localStorage.getItem(LOCAL_STORAGE_KEY);
-    if (db) {
-      const randomlyDB = JSON.parse(db);
-
-      localStorage.setItem(
-        LOCAL_STORAGE_KEY,
-        JSON.stringify({
-          ...randomlyDB,
-          items: randomlyDB.items.map((item: Item) => {
-            if (item.id === id) {
-              return { ...item, ...updateItem };
-            } else {
-              return item;
-            }
-          }),
-        })
-      );
-    }
+    updateLocalData({
+      items: getLocalData().items.map((item: Item) => {
+        if (item.id === id) {
+          return { ...item, ...updateItem };
+        } else {
+          return item;
+        }
+      }),
+    });
   };
 }
