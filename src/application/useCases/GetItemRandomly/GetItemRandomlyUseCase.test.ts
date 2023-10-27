95fd06fee5f4a53c7a8d59392dce442a20e50510
felixrigau <felixrigau88@gmail.com>
2023-10-27 13:10:41 +0200

    covert two test cases for getItemRandomly use case

---
+++ src/application/useCases/GetItemRandomly/GetItemRandomlyUseCase.test.ts
@@ -0,0 +1,47 @@
+import { GetItemRandomlyUseCase } from "./GetItemRandomlyUseCase";
+import { ItemRepositoryMock } from "../../model/ItemRepository.mock";
+import { VisitedItemRepositoryMock } from "../../model/VisitedItemRepository.mock";
+import { ItemRespository } from "../../model/IItemRepository";
+import { Item } from "../../model/Item";
+import { IVisitedItemRespository } from "../../model/IVisitedItemRespository";
+
+describe("get all items use case - tests suite", () => {
+  const itemRespository: jest.Mocked<ItemRespository> =
+      new ItemRepositoryMock(),
+    visitedItemRespository: jest.Mocked<IVisitedItemRespository> =
+      new VisitedItemRepositoryMock();
+
+  test("execute method should throw a error if there are not element to return", () => {
+    itemRespository.getAll.mockImplementation(() => []);
+
+    const getItemRandomly = new GetItemRandomlyUseCase(
+      itemRespository,
+      visitedItemRespository
+    );
+
+    expect(() => getItemRandomly.execute()).toThrow("There is no items");
+  });
+
+  test("execute method should return a random item that has not been visited yet", () => {
+    const item1 = new Item("abc"),
+      visitedItem = new Item("foo");
+
+    itemRespository.getAll.mockImplementation(() => [item1, visitedItem]);
+
+    visitedItemRespository.save(visitedItem.id);
+
+    const getItemRandomly = new GetItemRandomlyUseCase(
+      itemRespository,
+      visitedItemRespository
+    );
+
+    expect(getItemRandomly.execute().id).toBe(item1.id);
+  });
+  test("execute method should try many times to get a random item until finding a no visited one", () => {
+    expect(true).toBe(false);
+  });
+
+  // test("execute method should ", () => {
+  //   expect(true).toBe(false);
+  // });
+});
---
+++ src/application/useCases/GetItemRandomly/GetItemRandomlyUseCase.ts
@@ -0,0 +1,23 @@
+import { ItemRespository } from "../../model/IItemRepository";
+import { IVisitedItemRespository } from "../../model/IVisitedItemRespository";
+import { Item } from "../../model/Item";
+import { getBoundedRandomNumber } from "../../utilities/getBoundedRandomNumber";
+
+export class GetItemRandomlyUseCase {
+  constructor(
+    private itemRepository: ItemRespository,
+    private visitedItemRespository: IVisitedItemRespository
+  ) {}
+
+  execute(): Item {
+    const items = this.itemRepository.getAll();
+
+    if (!items.length) throw new Error("There is no items");
+
+    const randomIndex = getBoundedRandomNumber(0, items.length);
+
+    this.visitedItemRespository.exist(items[randomIndex].id);
+
+    return items[randomIndex];
+  }
+}
