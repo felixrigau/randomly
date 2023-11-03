6b97e6c6c166516b7f42490a0e686d05456f75a9
felixrigau <felixrigau88@gmail.com>
2023-11-03 16:15:08 +0100

    remove unnecessary code

--- src/infra/web-app/App.tsx
+++ src/infra/web-app/App.tsx
@@ -1,16 +1,4 @@
-import { Item } from "../../application/model/Item";
-import { CreateItemUseCase } from "../../application/useCases/CreateItem/CreateItemUseCase";
-import { GetAllItemsUseCase } from "../../application/useCases/GetAllItems/GetAllItemsUseCase";
-import { ItemStorageRepository } from "../adapters/ItemStorageRepository/ItemStorageRepository";
 import CreateForm from "./components/CreateForm/createForm";
-
-const repository = new ItemStorageRepository();
-const createItem = new CreateItemUseCase(repository);
-createItem.execute(new Item("Lissy"));
-createItem.execute(new Item("Mi familia1"));
-
-const getAllItems = new GetAllItemsUseCase(repository);
-const list = getAllItems.execute();
 
 export const App = () => (
   <>
