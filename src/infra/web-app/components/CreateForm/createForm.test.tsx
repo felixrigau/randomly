290627c40757ffc634094b3b39e54c2f686cfacc
felixrigau <felixrigau88@gmail.com>
2023-11-03 14:22:53 +0100

    create simple form and add testing

--- src/infra/web-app/components/CreateForm/createForm.test.tsx
+++ src/infra/web-app/components/CreateForm/createForm.test.tsx
@@ -5,6 +5,24 @@ describe("createForm - tests suite", () => {
   test("should have a input to write the item title", () => {
     render(<CreateForm />);
 
-    expect(screen.getByRole("textbox")).toBeInTheDocument();
+    expect(screen.getByLabelText("Title")).toBeInTheDocument();
+  });
+
+  test("should have a input to write the item description", () => {
+    render(<CreateForm />);
+
+    expect(screen.getByLabelText("Description")).toBeInTheDocument();
+  });
+
+  test("should have a checkbox to mark the item as fixed", () => {
+    render(<CreateForm />);
+
+    expect(screen.getByLabelText("IsFixed")).toBeInTheDocument();
+  });
+
+  test("should have a button to create the new button", () => {
+    render(<CreateForm />);
+
+    expect(screen.getByRole("button")).toBeInTheDocument();
   });
 });
--- src/infra/web-app/components/CreateForm/createForm.tsx
+++ src/infra/web-app/components/CreateForm/createForm.tsx
@@ -1,6 +1,12 @@
 const CreateForm = () => (
   <div>
-    <input type="text" />
+    <label htmlFor="title">Title</label>
+    <input id="title" type="text" />
+    <label htmlFor="description">Description</label>
+    <input id="description" type="text" />
+    <label htmlFor="isFixed">IsFixed</label>
+    <input id="isFixed" type="checkbox" />
+    <button>Create</button>
   </div>
 );
 
