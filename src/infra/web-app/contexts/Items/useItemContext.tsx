63642edf5fbef6c3c3e3b15307540ac00d187eb9
felixrigau <felixrigau88@gmail.com>
2023-11-06 18:22:42 +0100

    creating hook for accessing to context

---
+++ src/infra/web-app/contexts/Items/useItemContext.tsx
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import ItemsContext from "./itemContext";
+
+export const useItemsContext = () => {
+  const contextValue = useContext(ItemsContext);
+
+  if (!contextValue)
+    throw new Error(
+      "You are trying to access a context without being inside its scope"
+    );
+
+  return { ...contextValue };
+};
