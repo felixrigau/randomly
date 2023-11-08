62d6f8d5255bace3854d5b3d4d7f5fed11cde2d3
felixrigau <felixrigau88@gmail.com>
2023-11-08 06:23:06 +0100

    Implementing remove item use case

--- src/infra/web-app/components/itemList/itemList.test.tsx
+++ src/infra/web-app/components/itemList/itemList.test.tsx
@@ -4,8 +4,11 @@ import ItemList from "./itemList";
 import { GetAllItemsUseCase } from "../../../../application/useCases/GetAllItems/GetAllItemsUseCase";
 import { Item } from "../../../../application/model/Item";
 import { ItemsProvider } from "../../contexts/Items/itemContext";
+import { RemoveItemUseCase } from "../../../../application/useCases/RemoveItem/RemoveItemUseCase";
+import userEvent from "@testing-library/user-event";
 
 jest.mock("../../../../application/useCases/GetAllItems/GetAllItemsUseCase");
+jest.mock("../../../../application/useCases/RemoveItem/RemoveItemUseCase");
 
 describe("itemList - tests suite", () => {
   test("should show as many items exist", () => {
@@ -37,5 +40,34 @@ describe("itemList - tests suite", () => {
 
     expect(screen.queryAllByRole("listitem").length).toBe(0);
     expect(screen.getByText("There are no items")).toBeInTheDocument();
+  });
+
+  test("clicking remove item button should remove the item and update the items list", async () => {
+    const item = new Item("abc");
+    const getAllItemsMock = jest.fn().mockImplementation((): Item[] => [item]);
+    const removeItemMock = jest.fn();
+    (GetAllItemsUseCase as jest.Mock).mockImplementation(() => {
+      return {
+        execute: getAllItemsMock,
+      };
+    });
+    (RemoveItemUseCase as jest.Mock).mockImplementation(() => {
+      return {
+        execute: removeItemMock,
+      };
+    });
+
+    render(
+      <ItemsProvider>
+        <ItemList />
+      </ItemsProvider>
+    );
+
+    const removeButton = screen.getByRole("button", { name: "remove item" });
+
+    await userEvent.click(removeButton);
+
+    expect(getAllItemsMock).toBeCalledTimes(2);
+    expect(removeItemMock).toBeCalledWith(item.id);
   });
 });
