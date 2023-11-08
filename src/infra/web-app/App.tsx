24f8944a98499e7963a91d946dce75156985e064
felixrigau <felixrigau88@gmail.com>
2023-11-08 07:16:12 +0100

    Using showItem component

--- src/infra/web-app/App.tsx
+++ src/infra/web-app/App.tsx
@@ -1,10 +1,16 @@
 import CreateItem from "./components/createItem/createItem";
 import ItemList from "./components/itemList/itemList";
+import ShowItem from "./components/showItem/showItem";
 import { ItemsProvider } from "./contexts/Items/itemContext";
 
 export const App = () => (
   <ItemsProvider>
-    <CreateItem />
-    <ItemList />
+    <span>
+      <ShowItem />
+    </span>
+    <span>
+      <CreateItem />
+      <ItemList />
+    </span>
   </ItemsProvider>
 );
