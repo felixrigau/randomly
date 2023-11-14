92dfbb1a9874aa4aa1f8aa4317cb9f3805b4663e
felixrigau <felixrigau88@gmail.com>
2023-11-14 17:35:52 +0100

    changing context scope

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./infra/web-app/App";
+import { ItemsProvider } from "./infra/web-app/contexts/Items/itemContext";
 
 // Clear the existing HTML content
 document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById("app"));
-root.render(<App />);
+root.render(
+  <ItemsProvider>
+    <App />
+  </ItemsProvider>
+);
