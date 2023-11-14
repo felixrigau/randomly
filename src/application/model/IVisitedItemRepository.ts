f4a040d22f83883177ae2383c6bebf1ce8beb073
felixrigau <felixrigau88@gmail.com>
2023-11-14 16:53:50 +0100

    Add remove method to repository

--- src/application/model/IVisitedItemRepository.ts
+++ src/application/model/IVisitedItemRepository.ts
@@ -1,6 +1,7 @@
 export interface IVisitedItemRepository {
   exist: (id: string) => boolean;
   save: (id: string) => void;
+  remove: (id: string) => void;
   clear: () => void;
   size: () => number;
   getLastRequestDate: () => Date;
--- src/application/model/VisitedItemRepository.mock.ts
+++ src/application/model/VisitedItemRepository.mock.ts
@@ -3,6 +3,7 @@ import { IVisitedItemRepository } from "./IVisitedItemRepository";
 export class VisitedItemRepositoryMock implements IVisitedItemRepository {
   exist = jest.fn();
   save = jest.fn();
+  remove = jest.fn();
   clear = jest.fn();
   size = jest.fn();
   getLastRequestDate = jest.fn();
