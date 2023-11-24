b25de24defa9f02a3c25f342811c80e5accb6a04
felixrigau <felixrigau88@gmail.com>
2023-11-24 11:57:46 +0100

    add styles to showItem

---
+++ src/infra/web-app/components/showItem/showItem.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledItem = styled.section`
+  width: 100vw;
+  height: 100vh;
+  background: #fff1c6;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
