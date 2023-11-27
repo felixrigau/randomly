0b17f1b4ce69ef8b2e8d4df84805b4ec128b7221
felixrigau <felixrigau88@gmail.com>
2023-11-27 21:06:50 +0100

    Including route and refactor App

--- src/infra/web-app/App.tsx
+++ src/infra/web-app/App.tsx
@@ -1,47 +1,25 @@
-import { useEffect, useRef } from "react";
-import CreateItem from "./components/createItem/createItem";
-import ItemList from "./components/itemList/itemList";
-import ShowItem from "./components/showItem/showItem";
-import { WereItemsVisitedToday } from "../../application/useCases/WereItemsVisitedToday/WereItemsVisitedToday";
-import { VisitedItemIdStorageRepository } from "../adapters/VisitedItemIdStorageRepository/VisitedItemIdStorageRepository";
-import { ClearPreviousVisitedItems } from "../../application/useCases/ClearPreviousVisitedItems/ClearPreviousVisitedItems";
-import { useItemsContext } from "./contexts/Items/useItemContext";
-import { SaveLastVisitDate } from "../../application/useCases/UpdateLastVisitDate/SaveLastVisitDate";
-import UpdateItem from "./components/updateItem/updateItem";
-import SideBar from "./components/sideBar/sideBar";
-import { Button } from "./components/menuButton/menuButton";
+import { StrictMode } from "react";
 import { ResetStyles } from "./App.styled";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { MainPage } from "./pages/main/main";
+import { ManageItems } from "./pages/manageItems/manageItems";
 
-const respository = new VisitedItemIdStorageRepository();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/items",
+    element: <ManageItems />,
+  },
+]);
 
 export const App = () => {
-  const { existItems, toggleSideBar, isSideBarOpen } = useItemsContext();
-  const useCases = useRef({
-    wereItemsVisitedToday: new WereItemsVisitedToday(respository),
-    clearPreviousVisitedItems: new ClearPreviousVisitedItems(respository),
-    saveLastVisitDate: new SaveLastVisitDate(respository),
-  });
-
-  useEffect(() => {
-    const wereItemsVisitedToday =
-      useCases.current.wereItemsVisitedToday.execute();
-
-    if (!wereItemsVisitedToday) {
-      useCases.current.clearPreviousVisitedItems.execute();
-      useCases.current.saveLastVisitDate.execute(new Date());
-    }
-  }, []);
-
   return (
-    <>
+    <StrictMode>
       <ResetStyles />
-      <ShowItem />
-      <Button onClick={toggleSideBar}>menu</Button>
-      <SideBar isOpen={isSideBarOpen}>
-        <CreateItem />
-        <ItemList />
-        <UpdateItem />
-      </SideBar>
-    </>
+      <RouterProvider router={router} />
+    </StrictMode>
   );
 };
