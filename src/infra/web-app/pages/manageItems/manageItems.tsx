f99e9001ac9aa0d8423a5179f902746b68603e97
felixrigau <felixrigau88@gmail.com>
2023-11-28 18:35:19 +0100

    Changing Manage Items structure

--- src/infra/web-app/pages/manageItems/manageItems.tsx
+++ src/infra/web-app/pages/manageItems/manageItems.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import CreateItem from "../../components/createItem/createItem";
 import ItemList from "../../components/itemList/itemList";
 import UpdateItem from "../../components/updateItem/updateItem";
+import Modal from "../../components/modal/modal";
 
 export const ManageItems = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <>
       <ItemList />
-      <CreateItem />
       <UpdateItem />
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <CreateItem />
+      </Modal>
+      <button onClick={() => setIsModalOpen(true)}>Create</button>
     </>
   );
 };
