b6d99ad145b9659aa3da767644eebe38a6fbc0cd
felixrigau <felixrigau88@gmail.com>
2023-11-28 18:32:58 +0100

    Creating modal structure

---
+++ src/infra/web-app/components/modal/modal.tsx
@@ -0,0 +1,39 @@
+import { MouseEvent, PropsWithChildren, useEffect, useRef } from "react";
+import { Dialog } from "./modal.styled";
+
+type ModalType = {
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+const Modal = ({ children, isOpen, onClose }: PropsWithChildren<ModalType>) => {
+  const modalRef = useRef<HTMLDialogElement>();
+
+  useEffect(() => {
+    if (isOpen) {
+      modalRef.current.showModal();
+      document.body.style.overflow = "hidden";
+    } else {
+      modalRef.current.close();
+      document.body.style.overflow = "scroll";
+    }
+
+    return () => {
+      document.body.style.overflow = "scroll";
+    };
+  }, [isOpen]);
+
+  const handleClick = (event: MouseEvent) => {
+    console.log(typeof event.target);
+    if (modalRef.current.contains(event.target as Node)) return;
+    modalRef.current.close();
+  };
+
+  return (
+    <Dialog ref={modalRef} onClick={handleClick} onClose={onClose}>
+      {children}
+    </Dialog>
+  );
+};
+
+export default Modal;
