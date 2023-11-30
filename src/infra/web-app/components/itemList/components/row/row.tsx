946e44c204c363b39be75544409b5c21743a7a77
felixrigau <felixrigau88@gmail.com>
2023-11-30 10:55:54 +0100

    use item crud hook from Row component

--- src/infra/web-app/components/itemList/components/row/row.tsx
+++ src/infra/web-app/components/itemList/components/row/row.tsx
@@ -1,31 +1,18 @@
-import { useRef } from "react";
 import { Item } from "../../../../../../application/model/Item";
-import { GetAllItemsUseCase } from "../../../../../../application/useCases/GetAllItems/GetAllItemsUseCase";
-import { RemoveItemUseCase } from "../../../../../../application/useCases/RemoveItem/RemoveItemUseCase";
-import { RemoveVisitedItemIdUseCase } from "../../../../../../application/useCases/RemoveVisitedItemId/RemoveVisitedItemIdUseCase";
 import { useItemsContext } from "../../../../contexts/Items/useItemContext";
-import { ItemStorageRepository } from "../../../../../adapters/ItemStorageRepository/ItemStorageRepository";
-import { VisitedItemIdStorageRepository } from "../../../../../adapters/VisitedItemIdStorageRepository/VisitedItemIdStorageRepository";
+import useItemCRUD from "../../../../hooks/useItemCRUD/useItemCRUD";
 
 type RowType = {
   item: Item;
 };
 
-const itemRepository = new ItemStorageRepository();
-const visitedItemRepository = new VisitedItemIdStorageRepository();
-
 const Row = ({ item }: RowType) => {
   const { setItems, setItem } = useItemsContext();
-  const useCases = useRef({
-    getAllItems: new GetAllItemsUseCase(itemRepository),
-    removeItem: new RemoveItemUseCase(itemRepository),
-    removeVisitedItemId: new RemoveVisitedItemIdUseCase(visitedItemRepository),
-  });
+  const { remove, getAll } = useItemCRUD();
 
   const removeItemBy = (id: string) => {
-    useCases.current.removeItem.execute(id);
-    useCases.current.removeVisitedItemId.execute(id);
-    setItems(useCases.current.getAllItems.execute());
+    remove(id);
+    setItems(getAll());
   };
 
   return (
