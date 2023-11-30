b7f3cd606ea78a953a0542accd702fbee047ba72
felixrigau <felixrigau88@gmail.com>
2023-11-30 11:01:24 +0100

    Passing button as children to Row component

--- src/infra/web-app/components/itemList/components/row/row.tsx
+++ src/infra/web-app/components/itemList/components/row/row.tsx
@@ -1,29 +1,15 @@
+import { PropsWithChildren } from "react";
 import { Item } from "../../../../../../application/model/Item";
-import { useItemsContext } from "../../../../contexts/Items/useItemContext";
-import useItemCRUD from "../../../../hooks/useItemCRUD/useItemCRUD";
 
 type RowType = {
   item: Item;
 };
 
-const Row = ({ item }: RowType) => {
-  const { setItems, setItem } = useItemsContext();
-  const { remove, getAll } = useItemCRUD();
-
-  const removeItemBy = (id: string) => {
-    remove(id);
-    setItems(getAll());
-  };
-
+const Row = ({ children, item }: PropsWithChildren<RowType>) => {
   return (
     <div role="listitem">
       {item.title}
-      <button aria-label="remove item" onClick={() => removeItemBy(item.id)}>
-        X
-      </button>
-      <button aria-label="edit item" onClick={() => setItem(item)}>
-        Edit
-      </button>
+      {children}
     </div>
   );
 };
