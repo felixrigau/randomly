33fb3b101dbaa53c37fecdcb0af0629d4616c75e
felixrigau <felixrigau88@gmail.com>
2023-12-07 13:44:21 +0100

    Create shared styles file with button container

---
+++ src/infra/web-app/pages/shared/styles.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledButtonContainer = styled.div`
+  position: fixed;
+  bottom: 3rem;
+  left: 0;
+  width: 100%;
+  display: flex;
+
+  & > button {
+    margin: auto;
+  }
+`;
