98d525379602a43ded7ab4b9cae2c7138e06b55d
felixrigau <felixrigau88@gmail.com>
2023-12-15 11:05:48 +0100

    Sorting by fixed items

--- src/application/useCases/GetAllItems/GetAllItemsUseCase.ts
+++ src/application/useCases/GetAllItems/GetAllItemsUseCase.ts
@@ -5,6 +5,18 @@ export class GetAllItemsUseCase {
   constructor(private repository: ItemRepository) {}
 
   execute(): Item[] {
-    return this.repository.getAll();
+    const items = this.repository.getAll();
+
+    const firstFixedItems = items.sort((a, b) => {
+      if (a.isFixed > b.isFixed) {
+        return -1;
+      } else if (a.isFixed < b.isFixed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return firstFixedItems;
   }
 }
