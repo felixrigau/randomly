daaeac2deb16c1ff134644f97ea8884e2ecece5f
felixrigau <felixrigau88@gmail.com>
2023-12-15 10:11:40 +0100

    Fix layout visual bug

--- src/infra/web-app/components/layout/layout.styled.ts
+++ src/infra/web-app/components/layout/layout.styled.ts
@@ -3,7 +3,7 @@ export const HEADER_HEIGHT = "4rem";
 
 export const StyledContainer = styled.div`
   display: grid;
-  grid-template-rows: auto 1fr;
+  grid-template-rows: 1fr;
 `;
 
 export const StyledMain = styled.main`
