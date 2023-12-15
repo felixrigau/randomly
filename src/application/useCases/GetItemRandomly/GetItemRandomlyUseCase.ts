a5450ba912d4b69b4d941c0549f59b561fad7cdd
felixrigau <felixrigau88@gmail.com>
2023-12-15 11:28:30 +0100

    Returning fixed items first

--- src/application/useCases/GetItemRandomly/GetItemRandomlyUseCase.ts
+++ src/application/useCases/GetItemRandomly/GetItemRandomlyUseCase.ts
@@ -2,6 +2,7 @@ import { ItemRepository } from "../../model/IItemRepository";
 import { IVisitedItemRepository } from "../../model/IVisitedItemRepository";
 import { Item } from "../../model/Item";
 import { getBoundedRandomNumber } from "../../utilities/getBoundedRandomNumber";
+import { GetAllItemsUseCase } from "../GetAllItems/GetAllItemsUseCase";
 import { NoMoreItemsError } from "./NoMoreItemsError";
 
 export class GetItemRandomlyUseCase {
@@ -11,11 +12,23 @@ export class GetItemRandomlyUseCase {
   ) {}
 
   execute(): Item {
-    const items = this.itemRepository.getAll();
+    const getAllUseCase = new GetAllItemsUseCase(this.itemRepository);
+    const items = getAllUseCase.execute();
 
     if (!items.length) return null;
     if (items.length === this.visitedItemRepository.size())
       throw new NoMoreItemsError();
+
+    let fixedIndex = 0;
+
+    while (items[fixedIndex].isFixed) {
+      if (this.visitedItemRepository.exist(items[fixedIndex].id)) {
+        fixedIndex++;
+      } else {
+        this.visitedItemRepository.save(items[fixedIndex].id);
+        return items[fixedIndex];
+      }
+    }
 
     let randomIndex;
 
@@ -23,9 +36,9 @@ export class GetItemRandomlyUseCase {
       randomIndex = getBoundedRandomNumber(0, items.length);
     } while (this.visitedItemRepository.exist(items[randomIndex].id));
 
-    const item = items[randomIndex];
-    this.visitedItemRepository.save(item.id);
+    const randomItem = items[randomIndex];
+    this.visitedItemRepository.save(randomItem.id);
 
-    return item;
+    return randomItem;
   }
 }
