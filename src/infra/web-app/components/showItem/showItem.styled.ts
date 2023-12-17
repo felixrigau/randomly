5af32216f21c850eec50be11435c72aad8a3f9f1
felixrigau <felixrigau88@gmail.com>
2023-12-17 12:32:57 +0100

    Centering show item content

--- src/infra/web-app/components/showItem/showItem.styled.ts
+++ src/infra/web-app/components/showItem/showItem.styled.ts
@@ -6,6 +6,8 @@ export const StyledItem = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 1rem;
+  text-align: center;
 `;
 
 export const Title = styled.p`
