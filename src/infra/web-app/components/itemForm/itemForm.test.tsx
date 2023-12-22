ac0b98fdbaf1f0cf8153e108a59b1f9ebbb9f970
felixrigau <felixrigau88@gmail.com>
2023-12-22 12:04:24 +0100

    Creating a new test case to validate that the order input is only for fixed elements

--- src/infra/web-app/components/itemForm/itemForm.test.tsx
+++ src/infra/web-app/components/itemForm/itemForm.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ItemForm from "./itemForm";
 import { ItemsProvider } from "../../contexts/Items/itemContext";
+import userEvent from "@testing-library/user-event";
 
 describe("createForm - tests suite", () => {
   test.only("should have all needed element to input the item data", () => {
@@ -13,7 +14,19 @@ describe("createForm - tests suite", () => {
 
     expect(screen.getByLabelText("Title")).toBeInTheDocument();
     expect(screen.getByLabelText("Text")).toBeInTheDocument();
-    expect(screen.getByLabelText("Order")).toBeInTheDocument();
     expect(screen.getByLabelText("IsFixed")).toBeInTheDocument();
+  });
+
+  test.only("should show the order input if the item is fixed", async () => {
+    render(
+      <ItemsProvider>
+        <ItemForm />
+      </ItemsProvider>
+    );
+
+    const isFixedCheckox = screen.getByLabelText("IsFixed");
+    await userEvent.click(isFixedCheckox);
+
+    expect(screen.getByLabelText("Order")).toBeInTheDocument();
   });
 });
