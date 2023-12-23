9d0e0832dc7133e8db69292734658785a622e4a7
felixrigau <felixrigau88@gmail.com>
2023-12-23 07:04:45 +0100

    Adding promises to Visited Items repository

--- src/application/model/IVisitedItemRepository.ts
+++ src/application/model/IVisitedItemRepository.ts
@@ -1,9 +1,9 @@
 export interface IVisitedItemRepository {
-  exist: (id: string) => boolean;
-  save: (id: string) => void;
-  remove: (id: string) => void;
-  clear: () => void;
-  size: () => number;
-  getLastRequestDate: () => Date;
-  saveLastRequestDate: (currentDate: Date) => void;
+  exist: (id: string) => Promise<boolean>;
+  save: (id: string) => Promise<void>;
+  remove: (id: string) => Promise<void>;
+  clear: () => Promise<void>;
+  size: () => Promise<number>;
+  getLastRequestDate: () => Promise<Date>;
+  saveLastRequestDate: (currentDate: Date) => Promise<void>;
 }
