91b1768ceaece616a1f7fd88a837db7e3e7ee0ce
felixrigau <felixrigau88@gmail.com>
2024-01-06 13:56:03 +0100

    removing unnecessary code

--- src/webApp/components/layout/layout.tsx
+++ src/webApp/components/layout/layout.tsx
@@ -2,8 +2,7 @@
 // There is a open pull request waiting for the check of the library author
 // https://github.com/jsdom/jsdom/pull/3403
 
-import { PropsWithChildren, useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { PropsWithChildren } from "react";
 import {
   StyledContainer,
   StyledFooter,
@@ -15,15 +14,7 @@ import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
 import HomeIcon from "@mui/icons-material/Home";
 
 export const Layout = ({ children }: PropsWithChildren) => {
-  const [isHome, setIsHome] = useState(false);
-  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-
-  const { pathname } = useLocation();
   const { goToHome, goToItems } = useGoTo();
-
-  useEffect(() => {
-    setIsHome(pathname === "/");
-  }, [pathname]);
 
   return (
     <StyledContainer>
